7307ab47a1bcd9a893762c2f7c25610f50799c53
Davey Melero <Davey1234576@gmail.com>
2023-06-02 15:42:21 -0700

    setup all files

--- client/src/pages/Calendar.jsx
+++ client/src/pages/Calendar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Button = () => {
+const Calendar = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const Button = () => {
   )
 }
 
-export default Button
+export default Calendar
